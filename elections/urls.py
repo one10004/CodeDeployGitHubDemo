b1ce08731605327e8c8b48c911217f9537362f39
one10004 <ryunhoo123@kookmin.ac.kr>
2018-03-25 21:22:22 +0900

    aa

--- elections/urls.py
+++ elections/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 #서버에 요청이 들어오면 어떻게 처리할지
 urlpatterns = [
 	url(r'^$', views.index),
+	url(r'^(?P<address>(\da-z/_-.)+)/submit$', views.gitwatch)
 ]
--- elections/views.py
+++ elections/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+from subprocess import call
 # Create your views here.
 
 def index(request):
 	return render(request,'elections/index.html')
+
+
+def gitwatch(request, address):
+	address = 'https://github.com/' + address
+	os.chdir('/home/ubuntu')
+	call('git clone ' + address)
+	words = address.split('/')
+	length = len(words)
+	os.chdir('/home/ubuntu/' + words[length-1])
+	call('python3 home/ubuntu/gitinspector/gitinspector.py -F html > home/ubuntu/CodeDeployGitHubDemo/elections/templates/elections/statistics.html')
+	return render(request, 'elections/statistics.html')
